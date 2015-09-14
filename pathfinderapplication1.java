18a4b2fed6b20fb4fa1351f66e81df1f4e00dfa4
Conor Thompson <conorthompson213@gmail.com>
2015-09-14 23:36:52 +0100

    Create pathfinderapplication1.java

---
+++ pathfinderapplication1.java
@@ -0,0 +1,20 @@
+@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pathfinderapplication1;
+
+/**
+ *
+ * @author Conor
+ */
+public class pathfinderapplication1 {
+    public static void main(String args[]){
+    PathFinderGUI myGUI = new PathFinderGUI();
+        myGUI.setVisible(true);
+    
+}
+}
